663a137036bd6d16f3172ffb1948917fa06ee957
Amrith Nayak <amrith92@gmail.com>
2014-08-06 20:32:47 +0530

    Added reach-out

---
+++ reach-out.php
@@ -0,0 +1,47 @@
+<?php require_once('header.php'); ?>
+<?php $active = 'reach-out'; ?>
+<?php require_once('menu.php'); ?>
+
+<div class="stories-container gap-please">
+    <div class="wrapper clearfix">
+        <div class="row">
+            <div class="col-2-3 blue-top">
+                <h1>Reach Out</h1>
+                <p>
+                    If you have any queries/suggestions or want to join the NSS
+                    club, please feel free to contact the coordinators listed
+                    here.
+                </p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-1-3 no-frills">
+                <div class="profile">
+                    <img src="img/prof-p-govindarajan.jpg" alt="Prof. P. Govindarajan">
+                    <div class="profile-text">
+                        <h3>Prof. Govindarajan P.</h3>
+                        <p>NSS Programme Coordinator</p>
+                        <p>+91 9940261620</p>
+                        <p>044 39931314</p>
+                        <p><a href="mailto:govindarajan.p@vit.ac.in">govindarajan.p@vit.ac.in</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-1-3 no-frills">
+                <div class="profile">
+                    <img src="img/prof-priya.jpg" alt="Prof. Priyaadharshini M.">
+                    <div class="profile-text">
+                        <h3>Prof. Priyaadharshini M.</h3>
+                        <p>NSS Faculty Officer</p>
+                        <p>+91 9677271150</p>
+                        <p>044 39931064</p>
+                        <p><a href="mailto:priyaadharshini.m@vit.ac.in">priyaadharshini.m@vit.ac.in</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once('footer.php'); ?>
