0729cc7188cd10a6abdcb48dad7856690a60bdb0
Amrith Nayak <amrith92@gmail.com>
2014-08-06 21:05:38 +0530

    Added FAQ page

---
+++ faq.php
@@ -0,0 +1,48 @@
+<?php require_once('header.php'); ?>
+<?php $active = 'faq'; ?>
+<?php require_once('menu.php'); ?>
+
+<div class="stories-container gap-please">
+    <div class="wrapper clearfix">
+        <div class="row">
+            <div class="col-2-3 blue-top">
+                <h1>Frequently Asked Questions</h1>
+            </div>
+        </div>
+
+        <ol class="faq-list">
+            <li class="whitewash blue-top">
+                <h2>What are the benefits of joining NSS at VIT Chennai?</h2>
+                <ul>
+                    <li>NSS is very essential for the overall development of students.</li>
+                    <li>Students with NSS certificates will be given weightage of admission to higher studies in   Foreign or other National Universities.</li>
+                    <li>NSS certificate holders will be given additional priority and weightage of additional points / bonus points in all PUBLIC SERVICE COMMISSION EXAMS</li>
+                    <li>Students learn to develop in themselves the quality of service to community and help the poor and needy.</li>
+                    <li>NSS inculcates the qualities of both head and heart, which is one of the essential parts of holistic development of students’ personality.</li>
+                    <li>NSS provides the volunteers with an opportunity to train themselves as the future leaders and decision makers of the country.</li>
+                    <li>It provides with opportunities to take part in inter-collegiate / inter-university / inter-state camps and exchange ideas with students from other colleges / universities / states.</li>
+                </ul>
+            </li>
+
+            <li class="whitewash blue-top">
+                <h2>What kind of activities can I take part in after I join?</h2>
+                <p>You can take part in any or all of:</p>
+                <ul>
+                    <li>Education-drive, social, developmental, community service programs at Campus and villages.</li>
+                    <li>Important weeks and national days like World Breast Feeding Week, International Youth Day, NSS Day, International Literacy Day, and National Integration Day, etc. are also celebrated by these units.</li>
+                    <li>The NSS Volunteers are also successfully participating in different training programs and National Integration Camps, etc. in other Universities with excellent performance. Blood detection camps, blood donation, Campus cleaning, Celebration of important days , Orientation camps, Participation of students in various seminars conducted all over the state,various cultural activities aiming at personality development of students, Raising of funds for various altruistic activities.</li>
+                </ul>
+            </li>
+
+            <li class="whitewash blue-top">
+                <h2>What unique opportunities can I get by joining NSS?</h2>
+                <p>The highlight of being in the NSS is definitely the Special Camping Programme.</p>
+                <p>
+                    Special camps of 10 days duration form an integral part of the NSS activities. The camps are conducted every year in villages or slums.The volunteers work in these regions for hygiene education, antiplastic campaign and to eradicate illiteracy. The camps are also aimed at the development of durable community assets like roads, canals, sanitation and other similar projects.
+                </p>
+            </li>
+        </ol>
+    </div>
+</div>
+
+<?php require_once('footer.php'); ?>
